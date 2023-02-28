6e52169d25334e918969868fb885089a9bca603b
Aida Golshanara <aida.golshanara@gmail.com>
2023-02-28 19:43:42 +0100

    complete the ListingItem component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import Moment from 'react-moment';
+import { FaTrash } from 'react-icons/fa';
+import { BiEdit } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 export default function ListingItem({ listing, id }) {
-  return <div>{listing.name}</div>;
+  return (
+    <li className='bg-white relative flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 ease-in-out m-[10px]'>
+      <Link className='contents' to={`/category/${listing.type}/${id}`}>
+        <img
+          className='h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in'
+          loading='lazy'
+          src={listing.imgUrls[0]}
+          alt=''
+        />
+        <Moment
+          className='absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg'
+          fromNow
+        >
+          {listing.timestamp?.toDate()}
+        </Moment>
+        <div className='w-full p-[10px]'>
+          <p className='font-semibold m-0 text-xl truncate'>{listing.name}</p>
+          <p className='text-[#457b9d] mt-2 font-semibold '>
+            $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            {listing.type === 'rent' && (
+              <span className='ml-1 text-xs'>per Month</span>
+            )}
+          </p>
+        </div>
+      </Link>
+    </li>
+  );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -152,8 +152,10 @@ export default function Profile() {
       <div className='max-w-6xl px-3 mx-auto'>
         {!loading && listings.length > 0 && (
           <>
-            <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
-            <ul>
+            <h2 className='text-2xl text-center font-semibold mt-6'>
+              My Listings
+            </h2>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
